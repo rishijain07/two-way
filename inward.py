4c4906f5518fbfae9f7d6ed73b91a2daad9122c0
rishijain07 <beingrishijain@gmail.com>
2023-10-22 21:34:22 +0530

    inward-sync implemented

--- inward.py
+++ inward.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 # MySQL Database Configuration
 db = pymysql.connect(
     host="localhost",
-    user="root",
+    user="rishi",
+    password="#Rishi123",
     database="zenskar",
     cursorclass=pymysql.cursors.DictCursor  # To return results as dictionaries
 )
@@ -24,34 +25,55 @@ print(producer.config)  # Print the Kafka producer configuration
 @app.route('/update_customer', methods=['POST'])
 def update_customer():
     data = request.get_json()
-    print(data)
+    # print(data)
     try:
         data = request.get_json()
         id = data.get('id')
         name = data.get('name')
         email = data.get('email')
         action = data.get('action')  # 'add' or 'update'
-        print(data)
+        # print(data)
 
         # Update local MySQL database
+        prev_name = ''
+        prev_email = ''
         if action == 'add':
             cursor.execute("INSERT INTO customers (id, name, email) VALUES (%s, %s, %s)", (id, name, email))
         elif action == 'update':
-            cursor.execute("UPDATE customers SET name = %s, email = %s WHERE ID = %s", (name, email, id))
+            cursor.execute("SELECT * FROM customers WHERE id = %s", (id,))
+            existing_customer = cursor.fetchone()
+
+            if existing_customer:
+                # If the customer exists, store the previous values
+                prev_name = existing_customer['name']
+                prev_email = existing_customer['email']
+
+                # Update the customer's information in the database
+                cursor.execute("UPDATE customers SET name = %s, email = %s WHERE id = %s", (name, email, id))
         else:
             return jsonify({"error": "str(e)"}), 500
 
         db.commit()
 
         # Produce Kafka message for the customer update
-        kafka_message = {
-            "id": id,
-            "name": name,
-            "email": email,
-            "action": action
-        }
+        kafka_message = {}
+        if action == 'add':
+            kafka_message = {
+                "id": id,
+                "name": name,
+                "email": email,
+                "action": action
+            }
+        elif action == 'update':
+            kafka_message = {
+                    "id": id,
+                    "prev": {"name": prev_name, "email": prev_email},
+                    "updated": {"name": name, "email": email},
+                    "action": action
+                }
+        print(kafka_message)
         try:
-            producer.send('customer-events', value=kafka_message)
+            producer.send('consumer-events', value=kafka_message)
         except Exception as kafka_error:
             print("Kafka send error:", kafka_error)
 
--- stripe-inward.py
+++ stripe-inward.py
@@ -1,5 +1,9 @@
 from confluent_kafka import Consumer, KafkaError
-import json  # Import the JSON module to parse the data
+import json
+import stripe
+
+# Initialize the Stripe API with your API key
+stripe.api_key = 'sk_test_51O41sQSAvARJ57IrqW1ilAFlNOZCne4bHFKHRmru9RgUbN7U3rUNAGuk4pIvqNfc3pzMjMO3VYGWFYnnDKpbDVmE00k9QtjKw5'
 
 # Kafka consumer configuration.
 kafka_consumer_config = {
@@ -8,7 +12,7 @@ kafka_consumer_config = {
     'auto.offset.reset': 'earliest'
 }
 consumer = Consumer(kafka_consumer_config)
-consumer.subscribe(['customer-events'])
+consumer.subscribe(['consumer-events'])
 
 while True:
     msg = consumer.poll(1.0)
@@ -18,7 +22,7 @@ while True:
 
     if msg.error():
         if msg.error().code() == KafkaError._PARTITION_EOF:
-            print('Reached end of partition')
+            print('Reached the end of the partition')
         else:
             print('Error while polling:', msg.error())
     else:
@@ -30,21 +34,47 @@ while True:
             data_str = data.decode('utf-8')
             data_dict = json.loads(data_str)
 
-            customer_id = data_dict.get('id')
-            name = data_dict.get('name')
-            email = data_dict.get('email')
+            # customer_id = data_dict.get('id')
+            # name = data_dict.get('name')
+            # email = data_dict.get('email')
             action = data_dict.get('action')
+
             print(data_dict)
 
             # Perform actions based on the data from Kafka
-            # For example, update the customer in your Stripe account.
-            # Note: You can uncomment and implement the Stripe logic.
-            # if action == 'create_update':
-            #     try:
-            #         stripe.Customer.modify(customer_id, name=name, email=email)
-            #         print(f'Updated customer {customer_id} in Stripe')
-            #     except stripe.error.StripeError as e:
-            #         print(f'Error updating customer {customer_id} in Stripe:', str(e))
+            if action == 'add':
+                try:
+                    # Create a new customer in Stripe
+                    name = data_dict.get('name')
+                    email = data_dict.get('email')
+                    customer = stripe.Customer.create(
+                        name=name,
+                        email=email
+                    )
+                    print(f'Created customer {customer.id} in Stripe')
+                except stripe.error.StripeError as e:
+                    print('Error creating customer in Stripe:', str(e))
+            elif action == 'update':
+                try:
+                    # Find the customer using previous email and name in Stripe
+                    # Note: You would need to implement a mechanism to uniquely identify customers in Stripe.
+                    # This may involve maintaining a mapping of your customer IDs to Stripe customer IDs.
+                    # Here, we assume you can uniquely identify customers in Stripe based on their email and name.
+                    prev_data = data_dict.get('prev')
+                    updated_data = data_dict.get('updated')
+                    customers = stripe.Customer.list(email=prev_data['email'])
+                    
+                    if len(customers.data) == 1:
+                        # Assuming only one match is found in Stripe
+                        customer = customers.data[0]
+                        # print(customer)
+                        # Update the customer's information in Stripe
+                        stripe.Customer.modify(customer.id, name=updated_data['name'], email=updated_data['email'])
+                        print(f'Updated customer {customer.id} in Stripe')
+                    else:
+                        print('No or multiple customers found for update in Stripe.')
+                except stripe.error.StripeError as e:
+                    print('Error updating customer in Stripe:', str(e))
 
         except json.JSONDecodeError as e:
             print('Error decoding JSON data:', str(e))
